548925ff253acef4c79df34ce5b30e60f8674d5d
Jeff Polasz <jeff_polasz@hotmail.com>
2018-02-06 01:33:12 -0500

    Added player jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float movementSpeed = 5f;
+	public float jumpPower = 10f;
+	public float secondJumpPower = 10f;
+	public Transform groundCheckPosition;
+	public float radius = 0.3f;
+	public LayerMask layerGround;
 
 	private Rigidbody myBody;
+	private bool isGrounded;
+	private bool playerJumped;
+	private bool canDoubleJump;
 
 	void Awake () {
 		myBody = GetComponent<Rigidbody> ();
@@ -15,10 +23,22 @@ public class PlayerMovement : MonoBehaviour {
 	// Is called every 4 frames
 	void FixedUpdate () {
 		PlayerMove ();
+		PlayerGrounded ();
+		PlayerJump ();
 	}
 
 	void PlayerMove () {
 		myBody.velocity = new Vector3 (movementSpeed, myBody.velocity.y, 0f);
+	}
+
+	void PlayerGrounded () {
+		isGrounded = Physics.OverlapSphere (groundCheckPosition.position, radius, layerGround).Length > 0;
+	}
+
+	void PlayerJump () {
+		if (Input.GetKey (KeyCode.Space) && isGrounded) {
+			myBody.AddForce (new Vector3 (0, jumpPower, 0));
+		}
 	}
 
 } // PlayerMovement
